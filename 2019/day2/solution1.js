924b6ee89dae6c2ec27dda6ec81a8e68f8d9fe07
Jason Baker <jbaker@freightwaves.com>
2019-12-04 23:40:24 -0500

    Day 2 solution 1.

---
+++ 2019/day2/solution1.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const codes = fs.readFileSync('day2input.txt','utf8').split(',').map(v=>+v);
+
+
+codes[1]=12;
+codes[2]=2
+
+
+for(let i=0; i<=codes.length; i+=4){
+    let instruction = codes[i];
+    if(instruction===99){
+        break;
+    }
+    else{
+        const operands = [codes[codes[i+1]], codes[codes[i+2]]];
+        let result;
+        if(instruction===1){
+            result = operands[0]+operands[1]
+        }
+        else if(instruction===2){
+            result = operands[0]*operands[1]
+        }
+        codes[codes[i+3]]=result
+    }
+
+}
+
+console.log(codes[0]);
