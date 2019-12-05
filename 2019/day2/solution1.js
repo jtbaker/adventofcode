da8e673ad149b9c4bc2a057be60fad42e49a8e03
Jason Baker <jbaker@freightwaves.com>
2019-12-04 23:43:36 -0500

    Wrap to function call.

--- 2019/day2/solution1.js
+++ 2019/day2/solution1.js
@@ -1,29 +1,33 @@
 const fs = require('fs');
 
-const codes = fs.readFileSync('day2input.txt','utf8').split(',').map(v=>+v);
+const sampleCodes = fs.readFileSync('day2input.txt','utf8').split(',').map(v=>+v);
 
 
 codes[1]=12;
 codes[2]=2
 
 
-for(let i=0; i<=codes.length; i+=4){
-    let instruction = codes[i];
-    if(instruction===99){
-        break;
-    }
-    else{
-        const operands = [codes[codes[i+1]], codes[codes[i+2]]];
-        let result;
-        if(instruction===1){
-            result = operands[0]+operands[1]
+function intCodes(codes){
+    for(let i=0; i<=codes.length; i+=4){
+        let instruction = codes[i];
+        if(instruction===99){
+            break;
         }
-        else if(instruction===2){
-            result = operands[0]*operands[1]
+        else{
+            const operands = [codes[codes[i+1]], codes[codes[i+2]]];
+            let result;
+            if(instruction===1){
+                result = operands[0]+operands[1]
+            }
+            else if(instruction===2){
+                result = operands[0]*operands[1]
+            }
+            codes[codes[i+3]]=result
         }
-        codes[codes[i+3]]=result
+
     }
 
+    return codes;
 }
 
-console.log(codes[0]);
+console.log(`The first item in the array wound up as: ${intCodes(sampleCodes)[0]}`);
