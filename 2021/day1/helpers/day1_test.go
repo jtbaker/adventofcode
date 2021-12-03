befc8bccf3cc59e5b176db97810d3933f625cbf0
Jason Baker <jbaker@freightwaves.com>
2021-12-03 10:06:47 -0600

    Update tests

--- 2021/day1/helpers/day1_test.go
+++ 2021/day1/helpers/day1_test.go
@@ -6,10 +6,19 @@ import (
 	"github.com/jtbaker/adventofcode/2021/day1/helpers"
 )
 
-func TestResults(t *testing.T) {
-	res := helpers.Solution("../input.txt")
+func TestLinesGreaterThanLast(t *testing.T) {
+	inputs := []float64{4.5, 8.0, 1.2, 9.4, 5.7}
+	res := helpers.FindLinesGreaterThanLast(inputs)
 
-	if res != 7 {
-		t.Errorf("The results should have equaled 7")
+	if len(res) != 2 {
+		t.Errorf("The results should have equaled 2")
+	}
+}
+
+func TestSum(t *testing.T) {
+	sum := helpers.Sum([]float64{1.3, 3.0, 9.1})
+
+	if target := float32(float64(13.4)); float32(sum) != target {
+		t.Errorf("Sum should have equaled %f, instead it was %f", target, sum)
 	}
 }
