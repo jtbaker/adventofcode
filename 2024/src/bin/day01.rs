8fc8acb797380c7768e3160f6a37fc18d13e64c8
Jason Baker <jason.th.baker@gmail.com>
2024-12-01 15:55:23 -0600

    day 1

--- 2024/src/bin/day01.rs
+++ 2024/src/bin/day01.rs
@@ -1,10 +1,33 @@
 use common;
 fn main() {
-    let input = common::read_input("data/inputs/01.txt");
-    println!("Solution for Day 01: {}", solve_day01(&input));
+    println!("Solution for Day 01: {}", solve_day01());
 }
 
-fn solve_day01(input: &str) -> String {
-    // Implement solution logic here
-    return input.to_string();
+fn solve_day01() -> i32 {
+    let input = common::read_input("data/inputs/01.txt");
+    let mut left_list = Vec::<i32>::new();
+    let mut right_list = Vec::<i32>::new();
+    // let mut diffs = Vec::<i32>::new();
+    let mut diff_sum: i32 = 0;
+    for line in input.lines() {
+        let mut row = line.split_whitespace();
+        if let(Some(first), Some(second)) = (row.next(), row.next()) {
+            match first.parse::<i32>() {
+                Ok(number)=> left_list.push(number),
+                Err(e)=> println!("Failed to parse number: {}", e)
+            }
+            match second.parse::<i32>() {
+                Ok(number)=> right_list.push(number),
+                Err(e)=> println!("Failed to parse number: {}", e)
+            }
+        }
+    }
+    left_list.sort();
+    right_list.sort();
+
+    for (i1, i2) in left_list.iter().zip(right_list.iter()) {
+        let diff = (i2 - i1).abs();
+        diff_sum += diff;
+    }
+    return diff_sum;
 }
--- 2024/src/bin/day02.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("It's day two!")
-}
